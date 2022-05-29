9ca110ca6b85339c2caff8e9ace241a3a9b192f7
JK-8989 <jiyoungkim365@gmail.com>
2022-05-29 15:07:54 -0700

    Add bg blur effect

---
+++ script.js
@@ -0,0 +1,23 @@
+const lightboxBg = document.createElement('div');
+lightboxBg.id = 'lightbox-bg';
+document.body.appendChild(lightboxBg);
+
+const images = document.querySelectorAll('img');
+
+images.forEach(images => {
+  images.addEventListener('click', ()=>{
+
+    lightboxBg.classList.add('active');
+    const fullImage = document.createElement('img');
+    fullImage.src = images.src;
+    fullImage.id = 'full-image';  
+
+    lightboxBg.appendChild(fullImage);
+       
+  })
+})
+
+lightboxBg.addEventListener('click', () => {
+  lightboxBg.classList.remove('active');
+})
+
