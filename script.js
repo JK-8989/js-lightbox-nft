3b0312c3a61e8b9a1f162955e8fb9ab9aa0f35a9
JK-8989 <jiyoungkim365@gmail.com>
2022-05-29 21:03:27 -0700

    Edit details of box-shadow

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const lightboxBg = document.createElement('div');
-lightboxBg.id = 'lightbox-bg';
+const lightboxBg = document.createElement("div");
+lightboxBg.id = "lightbox-bg";
 document.body.appendChild(lightboxBg);
 
-const images = document.querySelectorAll('img');
+const images = document.querySelectorAll("img");
 
-images.forEach(images => {
-  images.addEventListener('click', ()=>{
-
-    lightboxBg.classList.add('active');
-    const fullImage = document.createElement('img');
+images.forEach((images) => {
+  
+  images.addEventListener("click", () => {
+    lightboxBg.classList.add("active");
+    const fullImage = document.createElement("img");
     fullImage.src = images.src;
-    fullImage.id = 'full-image';  
+    fullImage.id = "full-image";
 
     lightboxBg.appendChild(fullImage);
-  })
-})
+  });
 
-lightboxBg.addEventListener('click', () => {
-  lightboxBg.classList.remove('active');
-})
-
+    lightboxBg.addEventListener("click", () => {
+    lightboxBg.classList.remove("active");
+  });
+});
